0a51c29b8e811beca55c7d7aa7306a5988296661
Deighton49 <deighton49@gmail.com>
2021-07-18 14:01:35 +0300

    Add files via upload

---
+++ perc.py
@@ -0,0 +1,4 @@
+a = int(input(' enter your math result: \n' ))
+b = int(input('enter your history result: \n '))
+c = int(input('enter your biology result: \n '))
+print('your result is \n',(a+b+c)/300*100, '%') 
